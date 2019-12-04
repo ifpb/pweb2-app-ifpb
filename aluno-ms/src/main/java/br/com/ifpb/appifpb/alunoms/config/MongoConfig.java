4206b0d272ff8fee6004dcfac4e85645fabf0943
MurilloTavares <murillo_eum@hotmail.com>
2019-12-04 10:32:21 -0300

    refatorando MongoConfig

--- aluno-ms/src/main/java/br/com/ifpb/appifpb/alunoms/config/MongoConfig.java
+++ aluno-ms/src/main/java/br/com/ifpb/appifpb/alunoms/config/MongoConfig.java
@@ -1,24 +1,23 @@
 package br.com.ifpb.appifpb.alunoms.config;
 
 import br.com.ifpb.appifpb.alunoms.repository.AlunoRepository;
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories(basePackageClasses = AlunoRepository.class)
 @Configuration
-public class MongoConfig extends AbstractMongoConfiguration {
+public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Override
     protected String getDatabaseName() {
         return "aluno-ms";
     }
 
-    @Override
-    public MongoClient mongoClient() {
-        return new MongoClient("mongo", 27017);
-    }
 
+    @Override
+    public com.mongodb.client.MongoClient mongoClient() {
+        return MongoClients.create("mongodb://mongo:27017");
+    }
 }
