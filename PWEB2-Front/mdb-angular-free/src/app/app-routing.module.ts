b6493dcbc19e5e7d455a0dd5868ef123acff65e4
Raul-Coelho <raulcc50@gmail.com>
2019-12-05 15:01:07 -0300

    Configurando rota de cadastro e perfil

--- PWEB2-Front/mdb-angular-free/src/app/app-routing.module.ts
+++ PWEB2-Front/mdb-angular-free/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './login/login.module#LoginModule' },
   { path: 'login', loadChildren: './login/login.module#LoginModule' },
-  // { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroModule'},
-  // { path: 'aluno', loadChildren: './aluno/aluno.module#AlunoModule'}
+  { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroModule'},
+  { path: 'aluno', loadChildren: './aluno/aluno.module#AlunoModule'}
 ];
 
 @NgModule({
