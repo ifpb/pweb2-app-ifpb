036de51f5e49acd891f1ae3b6b82fe999beb6a60
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2019-12-09 14:01:27 -0300

    download e persistência dos dados de bolsas estudantis e projetos de estenção e de pesquisa

--- data_processor_ms/data_processor.py
+++ data_processor_ms/data_processor.py
@@ -7,17 +7,36 @@ import schedule
 from time import sleep
 
 
-
 def download_dados():
     print('Iniciando download dos dados.\n...')
-    nomes = ['alunos.csv', 'campi.csv']
+    nomes = ['alunos.csv', 'campi.csv', 'bolsas.csv', 'projetos-extensao.csv', 'projetos-pesquisa.csv']
     reqs = [requests.get('https://dados.ifpb.edu.br/dataset/d02eb6b8-5745-4436-ae22-ef1c182897d9/resource/29c2b593-ed14-4b73-b30c-d6135f072cf7/download/alunos.csv'),
-            requests.get('https://dados.ifpb.edu.br/dataset/096a37b3-52c6-43d6-9ec3-6de5b73b51a0/resource/77679e8c-f885-4b62-bf33-df7cc55b2bb9/download/campi.csv')]
+            requests.get('https://dados.ifpb.edu.br/dataset/096a37b3-52c6-43d6-9ec3-6de5b73b51a0/resource/77679e8c-f885-4b62-bf33-df7cc55b2bb9/download/campi.csv'),
+            requests.get('https://dados.ifpb.edu.br/dataset/5e8c6175-55c2-4498-b51b-7b45ae16ddcd/resource/a93651da-2257-42ed-ac7c-057b6db4fa3c/download/bolsas.csv'),
+            requests.get('https://dados.ifpb.edu.br/dataset/029b50a4-f50a-422d-867f-b457277b5168/resource/7b3b7850-9a34-450d-96e1-e7ce13f4bb9f/download/projetos-extensao.csv'),
+            requests.get('https://dados.ifpb.edu.br/dataset/e99b5cfd-f2f3-4b54-bb4f-6ddd9e480af7/resource/c45ea036-d8fc-4ba9-88bc-7854d5b964d5/download/projetos-pesquisa.csv')
+            ]
 
     for req, nome in zip(reqs, nomes):
         with open('/app/DB/dados/'+nome, 'wb') as csv:
             csv.write(req.content)
     print('Fim do download.')
+
+
+def tratando_csv_bolsas():
+    bolsas = pandas.read_csv('/app/DB/dados/bolsas.csv')
+    bolsas['categoria_bolsa'] = bolsas['categoria_bolsa'].str.strip()
+    return bolsas
+
+
+def tratando_csv_projetos_extensao():
+    projetos_extensao = pandas.read_csv('/app/DB/dados/projetos-extensao.csv')
+    return projetos_extensao
+
+
+def tratando_csv_projetos_pesquisa():
+    projetos_pesquisa = pandas.read_csv('/app/DB/dados/projetos-pesquisa.csv')
+    return projetos_pesquisa
 
 
 def tratando_csv_aluno():
@@ -52,6 +71,9 @@ def persistir():
     print('Persistindo dados.')
     engine = create_engine('postgresql+psycopg2://postgres:postgres@db_postgres:5432/tratador_db')
     tratando_csv_aluno().to_sql(name='alunos', con=engine, if_exists='replace', index=True, index_label='id')
+    tratando_csv_bolsas().to_sql(name='bolsas', con=engine, if_exists='replace', index=True, index_label='id')
+    tratando_csv_projetos_extensao().to_sql(name='projetos_extensao', con=engine, if_exists='replace', index=True, index_label='id')
+    tratando_csv_projetos_pesquisa().to_sql(name='projetos_pesquisa', con=engine, if_exists='replace', index=True, index_label='id')
     print('Persistência concluida.')
 
 
@@ -64,6 +86,7 @@ def agendador():
 
 
 def run():
+	print("run...")
 	persistir()
 	agendador()
 	thread = Thread(target=agendador)
