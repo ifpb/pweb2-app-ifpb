6d463847344a7caf74a0f3f41f4479c573616bc6
Raul-Coelho <raulcc50@gmail.com>
2020-02-11 12:45:22 -0300

    Add service login

---
+++ PWEB2-Front/mdb-angular-free/src/app/services/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Token } from '@angular/compiler/src/ml_parser/lexer';
+import { Login } from '../models/Login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private url = environment.host + 'login';
+  constructor(private http: HttpClient) { }
+  
+  login(login : Login) : Observable<HttpResponse<Token>>{
+    return this.http.post<Token>(this.url, login, { observe : 'response'});
+  }
+}
