d74ba1c5e899db9c9fa388d7b5b3e48b9ac567d1
MailsonD <mailssondennis@gmail.com>
2020-02-12 11:46:38 -0300

    Criando e configurando Api-gateway

---
+++ Api-Gateway/src/main/java/com/pweb2/appifpb/ApiGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.pweb2.appifpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableFeignClients
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ Api-Gateway/src/test/java/com/pweb2/appifpb/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pweb2.appifpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
