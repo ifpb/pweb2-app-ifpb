f7aec42d28c1e3808cb5205faa80544d3d877198
Raul-Coelho <raulcc50@gmail.com>
2020-02-20 17:47:24 -0300

    Configurando binding de cadastro

--- PWEB2-Front/mdb-angular-free/src/app/cadastro/cadastro.component.ts
+++ PWEB2-Front/mdb-angular-free/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from '../models/Login';
+import { AlunoService } from '../services/aluno.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  login: Login = new Login();
+
+  constructor(
+    private alunoService: AlunoService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
 
+  cadastrarAluno() {
+    this.alunoService.salvar(this.login).subscribe(
+      res => {
+        console.log("foi");
+        Swal.fire({
+          icon: 'success',
+          title: 'Cadastrado com sucesso!',
+          text: 'retornando ao login',
+          confirmButtonColor:'#00FF00'
+        }).then(result =>{
+          this.router.navigate[('/login')];
+        }).catch(erro =>{
+          console.log(erro);
+        })
+      },
+      erro => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Não foi possivel efetuar cadastro!',
+          text: 'Tente novamente',
+          confirmButtonColor:'#FF8BAB'
+        })
+      });
+    }
 }
