7ae546b938ab238ca98ff49b7c60bd0a4b58fa94
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-02-20 17:35:12 -0300

    Controlador de campi-ms

---
+++ campi-ms/src/main/java/br/com/ifpb/appifpb/campims/controller/CampiDesistenteController.java
@@ -0,0 +1,32 @@
+package br.com.ifpb.appifpb.campims.controller;
+
+
+import br.com.ifpb.appifpb.campims.dto.CampiComPercentualDTO;
+import br.com.ifpb.appifpb.campims.dto.NomeCampiDTO;
+import br.com.ifpb.appifpb.campims.service.CampiDesistenteService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("campi")
+@AllArgsConstructor
+public class CampiDesistenteController {
+
+    private CampiDesistenteService campiService;
+
+    @GetMapping("all")
+    public ResponseEntity<List<NomeCampiDTO>> nomesCampi(){
+        return ResponseEntity.ok().body(this.campiService.nomesCampi());
+    }
+
+    @GetMapping("{nomeCampi}")
+    public ResponseEntity<CampiComPercentualDTO> buscarCampusPorNome(@PathVariable("nome") String nome){
+        return ResponseEntity.ok().body(this.campiService.buscarCampusPorNome(nome));
+    }
+}
--- campi-ms/src/main/java/br/com/ifpb/appifpb/campims/dto/CampiComPercentualDTO.java
+++ campi-ms/src/main/java/br/com/ifpb/appifpb/campims/dto/CampiComPercentualDTO.java
@@ -1,4 +1,17 @@
 package br.com.ifpb.appifpb.campims.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CampiComPercentualDTO {
+
+    private Integer id;
+    private String campus;
+    private Float quant;
+    private Float total;
+    private Float percentual;
 }
